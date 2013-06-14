18313c68285357d5ef95819c69898a7f8c5d58fb
Brian Han <brianyhan@gmail.com>
2013-06-13 17:18:47 -0700

    Added SASS

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+	<head>
+		<title>Spic N' Span Cleaners | Dry Cleaning | Orange County | Orange Circle | Cheap | Affordable | Quality</title>
+	 	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,400italic,600,600italic,700,700italic,800italic,800' rel='stylesheet' type='text/css'>
+   	<link href="stylesheets/bootstrap.css" rel="stylesheet" type="text/css" /> 
+   	<link href="stylesheets/bootstrap-responsive.css"  rel="stylesheet" type="text/css" /> 
+	</head>
+
+	<body><div class="container">
+		<div class="row">
+  <div class="span2">..ssss.</div>
+  <div class="span8">...</div>
+	</div>
+	<div class="page-header">LLLLLLLLL</div>
+</div>
+	</body>
+</html>
